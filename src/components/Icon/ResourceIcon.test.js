b4bb02073b3704eed2468956f20b0575617fa669
Dario Civallero <dario.civallero@gmail.com>
2018-02-01 21:17:44 +0100

    unit tests

---
+++ src/components/Icon/ResourceIcon.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import 'jest-enzyme'
+import ResourceIcon from './ResourceIcon'
+import StarWarsIcon from './StarWarsIcon'
+
+describe('ResourceIcon Component', () => {
+  it('renders without crashing', () => {
+    shallow(<ResourceIcon />)
+  })
+  it('renders the StarWarsIcon matching the resource prop', () => {
+    const starshipsIcon = shallow(<ResourceIcon resource="starships" />)
+    const peopleIcon = shallow(<ResourceIcon resource="people" />)
+    expect(starshipsIcon).toContainReact(<StarWarsIcon icon="swg-falcon" />)
+    expect(peopleIcon).toContainReact(<StarWarsIcon icon="swg-leia" />)
+  })
+  it("renders the StarWarsIcon with default value if the resource prop doesn't match any mapping", () => {
+    const defaultIcon = shallow(<ResourceIcon resource="not_match" />)
+    expect(defaultIcon).toContainReact(<StarWarsIcon icon="swg-starwars" />)
+  })
+})
---
+++ src/components/Icon/StarWarsIcon.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import 'jest-enzyme'
+import StarWarsIcon from './StarWarsIcon'
+
+describe('StarWarsIcon Component', () => {
+  it('renders without crashing', () => {
+    shallow(<StarWarsIcon />)
+  })
+  it('renders the icon passed in props', () => {
+    const component = shallow(<StarWarsIcon icon="some-icon" />)
+    expect(component).toContainReact(<i aria-hidden="true" className="swg some-icon" />)
+  })
+})
